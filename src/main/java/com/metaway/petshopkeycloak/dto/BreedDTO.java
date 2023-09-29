d7c9e31fe9f3ff78baf1ca4bbfc96d752dcc7d55
LorenzoBondan <lbbondan@gmail.com>
2023-09-29 19:57:04 -0300

    first commit

---
+++ src/main/java/com/metaway/petshopkeycloak/dto/BreedDTO.java
@@ -0,0 +1,66 @@
+package com.metaway.petshopkeycloak.dto;
+
+import com.metaway.petshopkeycloak.entities.Breed;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BreedDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    @NotBlank(message = "Required field")
+    private String description;
+    private final List<Long> petsId = new ArrayList<>();
+
+    public BreedDTO(){}
+
+    public BreedDTO(Breed entity){
+        this.id = entity.getId();
+        this.description = entity.getDescription();
+        this.petsId.clear();
+        entity.getPets().forEach(pet -> this.petsId.add(pet.getId()));
+    }
+
+    public BreedDTO(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Long> getPetsId() {
+        return petsId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BreedDTO breedDTO = (BreedDTO) o;
+        return Objects.equals(id, breedDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/metaway/petshopkeycloak/entities/Breed.java
@@ -0,0 +1,62 @@
+package com.metaway.petshopkeycloak.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_breed")
+public class Breed implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @OneToMany(mappedBy = "breed", fetch = FetchType.EAGER)
+    private Set<Pet> pets = new HashSet<>(); // List was duplicating pets from users with more than one role
+
+    public Breed(){}
+
+    public Breed(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Breed breed = (Breed) o;
+        return Objects.equals(id, breed.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
